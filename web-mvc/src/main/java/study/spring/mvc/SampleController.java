b2ad99eb1469114e71a814b29c55b96afde8e828
김윤수 <younsookim@tmon.co.kr>
2019-10-21 18:55:21 +0900

    working

--- web-mvc/src/main/java/study/spring/mvc/SampleController.java
+++ web-mvc/src/main/java/study/spring/mvc/SampleController.java
@@ -1,20 +1,60 @@
 package study.spring.mvc;
 
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@SessionAttributes("event")
 public class SampleController {
 
-	@GetMapping(value = "/hello",
-			consumes = "!" + MediaType.APPLICATION_JSON_VALUE
-	)
-	@ResponseBody
-	public String hello() {
-		return "hello";
+	@GetMapping("/events/form")
+	public String eventsForm(Model model, SessionStatus sessionStatus) {
+		Event newEvent = new Event();
+		newEvent.setLimit(10);
+		model.addAttribute("event", new Event());
+		sessionStatus.setComplete();
+		return "/events/form";
 	}
+
+	@PostMapping("/events")
+	public String createEvent(@Validated @ModelAttribute Event event,
+			BindingResult bindingResult,
+			Model model) {
+		if (bindingResult.hasErrors()) {
+			return "/events/form";
+		}
+
+		List<Event> eventList = new ArrayList<>();
+		eventList.add(event);
+		model.addAttribute(eventList);
+
+		return "redirect:/events/list";
+	}
+
+	@GetMapping("/events/list")
+	public String getEvents(Model model) {
+		Event event = new Event();
+		event.setName("spring");
+		event.setLimit(10);
+
+		List<Event> eventList = new ArrayList<>();
+		eventList.add(event);
+
+		model.addAttribute(eventList);
+
+		return "/events/list";
+	}
+
+
 }
--- web-mvc/src/test/java/study/spring/mvc/SampleControllerTest.java
+++ web-mvc/src/test/java/study/spring/mvc/SampleControllerTest.java
@@ -4,16 +4,19 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.servlet.ModelAndView;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.notNullValue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -24,11 +27,26 @@ class SampleControllerTest {
 
 	@Test
 	public void hello() throws Exception {
-		mockMvc.perform(get("/hello")
-					.contentType(MediaType.APPLICATION_XML)
-					.accept(MediaType.APPLICATION_JSON))
+		ResultActions resultActions = mockMvc.perform(get("/events")
+				.param("name", "younsoo")
+				.param("limit", "-10"))
 				.andDo(print())
 				.andExpect(status().isOk())
-				.andExpect(content().string("hello"));
+				.andExpect(model().hasErrors());
+		ModelAndView mav = resultActions.andReturn().getModelAndView();
+		Map<String, Object> model = mav.getModel();
+		System.out.println(model.size());
+	}
+
+	@Test
+	public void eventForm() throws Exception {
+		MockHttpServletRequest request = mockMvc.perform(get("/events/form"))
+				.andDo(print())
+				.andExpect(view().name("/events/form"))
+				.andExpect(model().attributeExists("event"))
+				.andExpect(request().sessionAttribute("event", notNullValue()))
+				.andReturn().getRequest();
+		Event event = (Event) request.getSession().getAttribute("event");
+		System.out.println(event);
 	}
 }
